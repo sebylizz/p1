94bfa81671e30960c6f88d7f1bb82040dbb84056
emilkjeldgaard <ekkr23@student.aau.dk>
2023-11-06 09:39:17 +0100

    Update

---
+++ cmake-build-debug/CMakeFiles/p1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p1.dir/src/input.c.o"
+  "CMakeFiles/p1.dir/src/input.c.o.d"
+  "p1"
+  "p1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/p1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
