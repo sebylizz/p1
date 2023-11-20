6e95241884dd1281c8b947917ea69fab6cbdcfa8
emilkjeldgaard <ekkr23@student.aau.dk>
2023-11-20 09:08:04 +0100

    Første checkfunktion

---
+++ src/input_test.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void bekraeft_cpr();
+
+int main(void) {
+
+    int cpr = bekraeft_cpr();
+
+return 0;
+
+}
+
+void bekraeft_cpr() {
+
+    char svar;
+    int cpr;
+    char * sqlnavn = "Emil Bøsrup";
+
+    do {
+
+        printf("Indtast CPR-nummer på patienten> \n");
+        scanf(" %d", &cpr);
+
+        printf("Er patientens navn %s og CPR-nummer %d korrekt? [y/n]\n", sqlnavn, cpr);
+        scanf(" %c", &svar);
+
+    } while (svar != 'y');
+
+    printf("Patientens navn er %s og CPR-nummer %d", sqlnavn, cpr);
+
+    return cpr;
+
+}
