3176e60cc626c7bf27b4e0c31f0744edefb7e77c
LukasTN03 <DL48WD@student.aau.dk>
2023-11-25 11:32:26 +0100

    Med comments

--- src/medicin_input.c
+++ src/medicin_input.c
@@ -15,30 +15,31 @@ int medicin();
     char s_time;
 
     do {
+        // Spørge om medicin
         // Skal kobles  sammen med første input funktion så der spørges om navnet herunder //
         printf("What medicine would you like to prescribe to \n");
         scanf(" %s", med_input);
-
+        // Check funktion
         printf("Is %s the correct medicine you wish to prescribe? [y/n]\n", med_input);
         scanf(" %c", &check1);
 
     } while (check1 != 'y');
 
-    do {
+    do { // spørge om styrke, evt. tilføje checks i forhold til grænser udfra database med bestemt medicin
         printf("What dosage of %s would you like to prescribe? (in mg)\n", med_input);
         scanf(" %d", &dosis);
 
-
+        // Check funktion
         printf("Is %d mg the correct dosage of %s you wish to prescribe? [y/n]\n", dosis, med_input);
         scanf(" %c", &check2);
 
 
     } while (check2 != 'y');
     char times[frek][6];
-    do {
+    do { // Spørge om frekvensen
         printf("How many times a day do you wish the patient to take %s with the dosage of %d mg\n", med_input, dosis);
         scanf(" %d", &frek);
-
+        // Check funktion
         printf("Is it correct that you wish the patient takes %s %d time(s) a day with the dosage of %d mg? [y/n]\n",
                med_input, frek, dosis);
         scanf(" %c", &check3);
@@ -46,7 +47,7 @@ int medicin();
     } while (check3 != 'y');
 
     //bi_medicin();
-    do {
+    do { // Spørge om det skal være bestemte tidspunkter eller en beskrivelse af hvornår det skal tages ca.
         printf("1. Do you wish for the patient to take the medicine at a specific time/times?\n");
         printf("2. Do you wish to give the patient a description of when to take the medicine? [1/2]\n");
         scanf(" %d", &check4);
@@ -55,22 +56,22 @@ int medicin();
 
     if (check4 == 1) {
         if (frek == 1) {
-            do {
+            do { // spørge om bestemt tidspunkt hvis frekvensen kun er 1
                 printf("At what time of day do you wish the patient takes %s (24 hour format, i.e 16:00)\n", med_input);
                 scanf(" %s", &s_time);
-
+                // Check funktion
                 printf("Is it correct that you wish for the patient to take %s at %s [y/n]\n", med_input, s_time);
                 scanf(" %c", &check5);
             } while (check5 != 'y');
         }
         else {
-            do{
+            do{ // spørge om flere tidspunkter da frekvensen er over 1
                 printf("State the specific times a day the patient should take %s,\nfrom first to last, one at the time (24 hour format, i.e 16:00)\n", med_input);
                 for (int i = 0; i < frek; i++){
                     scanf(" %s", times[i]);
                     //times[i]
                     printf("State the next time they should take %s\n", med_input);
-                }
+                }// Check funktion
                 printf("Is it correct that you wish for the patient to take %s at the following times: [y/n]\n", med_input);
                 for (int i = 0; i < frek; i++){
                     printf("%s\n", times[i]);
@@ -83,10 +84,11 @@ int medicin();
     }
     if (check4 == 2) {
         do{
-            char d_time[250];
+            char d_time[250]; // spørger om beskrivelsen
             printf("State your description of when the patient should take %s:\n", med_input);
             getchar();
             fgets(d_time, 250, stdin);
+            // Check funktion
             printf("Is the following description correct: %s [y/n]\n", d_time);
             scanf(" %c", &check7);
         }
@@ -95,7 +97,7 @@ int medicin();
 
 
 
-
+// printning af recept (ikke helt færdig)
 void print_recept(dosis, frek, med_input, s_time, times, d_time) {
     for(int i = 0; i < antalrecepts; i++){
 
