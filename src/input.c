0d1cba0399e85260f9e473fe3d8f1ba167de8661
sebylizz <sebastiankjandersen@gmail.com>
2023-11-29 11:41:23 +0100

    så er der insert boooiiiisss

--- src/input.c
+++ src/input.c
@@ -33,7 +33,7 @@ int main(void) {
         }
         else if (valg == 'i') {
             medicin();
-            print_recept(cur.cpr, cur.name, recepts[antalrecepts].medname, recepts[antalrecepts].notes, recepts[antalrecepts].dosage);
+            print_recept(cur.cpr, cur.name, recepts[antalrecepts-1].medname, recepts[antalrecepts-1].notes, recepts[antalrecepts-1].dosage);
         }
         else if (valg == 's') {
             int sletvalg;
@@ -160,7 +160,9 @@ void insert_recept(int medid, int dosis, int frek, char desk[250]){
     char sql[250];
     sprintf(sql, "INSERT INTO patmed(cpr, id, dosage, frequency, notes) VALUES('%s', %d, %d, %d, '%s')", cur.cpr, medid, dosis, frek, desk);
     sqlite3_exec(db, sql, NULL, 0, &err_msg);
-    antalrecepts = 1;
+    antalrecepts = 0;
+    free(recepts);
+    recepts = NULL;
     sprintf(sql, "SELECT medicine.medicine, patmed.dosage, patmed.frequency, patmed.notes FROM patmed JOIN medicine ON medicine.id = patmed.id WHERE patmed.cpr = '%s'", cur.cpr);
     sqlite3_exec(db, sql, recept_callback, 0, &err_msg);
 
