e28b5645c934e7adfdadec91b9ac8a3870c905d3
sebylizz <sebastiankjandersen@gmail.com>
2023-12-06 13:22:03 +0100

    color correc

--- src/helpers.c
+++ src/helpers.c
@@ -51,7 +51,7 @@ void textColor(char col){
             printf("\033[1;34m");
             break;
         default:
-            printf("\033[0;37m");
+            printf("\033[0m");
             break;
     }
 }
